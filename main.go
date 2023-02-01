9994a97e9fb3482ecf084a9f0e3356fcfe8e7b75
Lev Nefedov <nefedov.lewa@gmail.com>
2023-02-01 16:59:03 +0400

    created  templates and set up router

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
+
+func main() {
+	router = gin.Default()
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(http.StatusOK, "index.html", gin.H{
+			"title":"Home Page",
+		})
+	})
+
+	router.Run()
+}
+
